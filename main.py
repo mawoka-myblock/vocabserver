77085a25977f27bdf4c9a58fa8bce9ba5f488748
mawoka <mawoka@e.email>
2021-03-18 17:58:35 +0100

    Adding new words works!!!

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from flask import Flask, render_template, url_for, request, redirect, g
 from flask_sqlalchemy import SQLAlchemy
 import flask_login
+import os
 
 import flask
 app = flask.Flask(__name__)
 app.secret_key = 'hjfzguzhnuhrgh87jzuizfhij8junhrt86zth8jz7hdzgzfu7ur8hajtdsk8hggftae76sgufgud'  # Change this!
 
 
-
-
-
-@app.route('/api/add-post', methods = ['GET', 'POST', 'DELETE'])
-def user():
+@app.route('/api/add-list/<subject>/<classroom>/<id>', methods = ['GET', 'POST', 'DELETE'])
+def save(subject, classroom, id):
     if request.method == 'GET':
         """return the information for <user_id>"""
 
     if request.method == 'POST':
-        # changes
-        data = request.form['content']
-        f = open("testfile.txt", "a")
-        f.write(data)
-        f.close()
-
+        l1 = request.form['lone']
+        l2 = request.form['ltwo']
+        try:
+            f = open(os.path.join('./data', id + ".txt"), "a")
+            f.write(l1)
+            f.write(" : ")
+            f.write(l2)
+            f.write("\n")
+            f.close()
+            return redirect("/")
+        except:
+            return "error 39615"
     else:
         # POST Error 405 Method Not Allowed
         return "Error"
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
