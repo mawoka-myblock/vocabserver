10f89ff3d23fcb63d634f792679372d72e766996
mawoka <mawoka@e.email>
2021-03-19 09:02:42 +0100

    Cleaned up the code and added ability to q

---
+++ datahandler.py
@@ -0,0 +1,48 @@
+from __main__ import app
+import os
+from flask import request
+
+
+@app.route('/api/add-list/<subject>/<classroom>/<id>', methods = ['GET', 'POST', 'DELETE'])
+def savethings(subject, classroom, id):
+    if request.method == 'GET':
+        """return the information for <user_id>"""
+
+    if request.method == 'POST':
+        try:
+            os.mkdir("./data/" + classroom)
+            os.mkdir("./data/" + classroom + "/" + subject)
+            f = open(os.path.join('./data/' + classroom + '/' + subject, id + ".txt"), "a")
+            # f = open(os.path.join('./data' + classroom, id + ".txt"), "a")
+            l1 = request.form['lone']
+            l2 = request.form['ltwo']
+            f.write(l1)
+            f.write(" : ")
+            f.write(l2)
+            f.write("\n")
+            f.close()
+            return "Success"
+        except:
+            try:
+                f = open(os.path.join('./data/' + classroom + '/' + subject, id + ".txt"), "a")
+                # f = open(os.path.join('./data' + classroom, id + ".txt"), "a")
+                l1 = request.form['lone']
+                l2 = request.form['ltwo']
+                f.write(l1)
+                f.write(" : ")
+                f.write(l2)
+                f.write("\n")
+                f.close()
+                return "success"
+            except:
+                return "Error"
+
+
+@app.route('/api/read-list/<subject>/<classroom>/<id>')
+def readlists(subject, classroom, id):
+    try:
+        with open(os.path.join('./data/' + classroom + '/' + subject, id + '.txt'), "r" ) as f:
+            content = f.readline()
+        return content
+    except:
+        return "Error 128596335"
--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect, g
-from flask_sqlalchemy import SQLAlchemy
 import flask_login
-import os
+#import os
+#import stuff
+
 
 import flask
 app = flask.Flask(__name__)
 app.secret_key = 'hjfzguzhnuhrgh87jzuizfhij8junhrt86zth8jz7hdzgzfu7ur8hajtdsk8hggftae76sgufgud'  # Change this!
 
-
-@app.route('/api/add-list/<subject>/<classroom>/<id>', methods = ['GET', 'POST', 'DELETE'])
-def save(subject, classroom, id):
-    if request.method == 'GET':
-        """return the information for <user_id>"""
-
-    if request.method == 'POST':
-        l1 = request.form['lone']
-        l2 = request.form['ltwo']
-        try:
-            f = open(os.path.join('./data', id + ".txt"), "a")
-            f.write(l1)
-            f.write(" : ")
-            f.write(l2)
-            f.write("\n")
-            f.close()
-            return redirect("/")
-        except:
-            return "error 39615"
-    else:
-        # POST Error 405 Method Not Allowed
-        return "Error"
+import datahandler
 
 if __name__ == '__main__':
     app.run(debug=True)
