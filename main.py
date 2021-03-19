7a4d7b14dc3acd8f811003bd6095033d98f5ee27
mawoka <mawoka@e.email>
2021-03-19 10:21:02 +0100

    started with the auth-thing

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, url_for, request, redirect, g
 import flask_login
 #import os
 #import stuff
-
+import flask_login
 
 import flask
 app = flask.Flask(__name__)
+login_manager = flask_login.LoginManager()
 app.secret_key = 'hjfzguzhnuhrgh87jzuizfhij8junhrt86zth8jz7hdzgzfu7ur8hajtdsk8hggftae76sgufgud'  # Change this!
+login_manager.init_app(app)
+
 
 import datahandler
+import auth
 
 if __name__ == '__main__':
     app.run(debug=True)
