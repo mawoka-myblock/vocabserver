b809110dd996e53edb478745ea5c64fc938f107d
mawoka <mawoka@e.email>
2021-03-20 18:55:21 +0100

    Lats commit for 2day with working and clean auth

--- datahandler.py
+++ datahandler.py
@@ -3,10 +3,12 @@ from fastapi import Form
 
 global response
 response = "hallo"
+
+
 def save(subject, classroom, id, l1, l2):
     try:
-        os.mkdir("./data/" + classroom)
-        os.mkdir("./data/" + classroom + "/" + subject)
+        os.mkdir("./data/vocab/" + classroom)
+        os.mkdir("./data/vocab/" + classroom + "/" + subject)
         f = open(os.path.join('./data/' + classroom + '/' + subject, id + ".txt"), "a")
         # f = open(os.path.join('./data' + classroom, id + ".txt"), "a")
         f.write(l1)
@@ -15,9 +17,10 @@ def save(subject, classroom, id, l1, l2):
         f.write("\n")
         f.close()
         response = "Success"
+        print(response)
     except:
         try:
-            f = open(os.path.join('./data/' + classroom + '/' + subject, id + ".txt"), "a")
+            f = open(os.path.join('./data/vocab/' + classroom + '/' + subject, id + ".txt"), "a")
             # f = open(os.path.join('./data' + classroom, id + ".txt"), "a")
             f.write(l1)
             f.write(" : ")
@@ -25,15 +28,26 @@ def save(subject, classroom, id, l1, l2):
             f.write("\n")
             f.close()
             response = "Success"
+            print(response)
         except:
             response = "Error"
+            print(response)
+
+
+print(response)
 
 
 def read(subject, classroom, id):
     try:
-        with open(os.path.join('./data/' + classroom + '/' + subject, id + '.txt'), "r" ) as f:
+        with open(os.path.join('./data/vocab/' + classroom + '/' + subject, id + '.txt'), "r") as f:
             content = f.readline()
         response = content
         return content
     except:
         return "Error 128596335"
+
+
+def getcontent():
+    firstdir = os.listdir(path="data/vocab")
+    for i in firstdir:
+        print(i)
--- main.py
+++ main.py
@@ -2,109 +2,33 @@ from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, Form, Depends
 import datahandler
-import secrets
-
-# import users
-
+import auth
+from auth import User, SECRET
 app = FastAPI()
 
 response = datahandler.response
 
+jwt_authentication = auth.JWTAuthentication(secret=SECRET, lifetime_seconds=3600, tokenUrl="/auth/jwt/login")
 
-
-
-
-# @app.get("/users/me")
-# def read_current_user(username: str = Depends(users.get_current_username)):
-#    return {"username": username}
-
-
-import databases
-import sqlalchemy
-from fastapi import FastAPI, Request
-from fastapi_users import FastAPIUsers, models
-from fastapi_users.authentication import JWTAuthentication
-from fastapi_users.db import OrmarBaseUserModel, OrmarUserDatabase
-
-DATABASE_URL = "sqlite:///test.db"
-SECRET = "SECRET"
-metadata = sqlalchemy.MetaData()
-database = databases.Database(DATABASE_URL)
-
-
-class User(models.BaseUser):
-    pass
-
-
-class UserCreate(models.BaseUserCreate):
-    pass
-
-
-class UserUpdate(User, models.BaseUserUpdate):
-    pass
-
-
-class UserDB(User, models.BaseUserDB):
-    pass
-
-
-class UserModel(OrmarBaseUserModel):
-    class Meta:
-        tablename = "users"
-        metadata = metadata
-        database = database
-
-
-engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all(engine)
-
-user_db = OrmarUserDatabase(UserDB, UserModel)
-
-
-def on_after_register(user: UserDB, request: Request):
-    print(f"User {user.id} has registered.")
-
-
-def on_after_forgot_password(user: UserDB, token: str, request: Request):
-    print(f"User {user.id} has forgot their password. Reset token: {token}")
-
-
-def after_verification_request(user: UserDB, token: str, request: Request):
-    print(f"Verification requested for user {user.id}. Verification token: {token}")
-
-
-jwt_authentication = JWTAuthentication(secret=SECRET, lifetime_seconds=3600, tokenUrl="/auth/jwt/login")
-
-fastapi_users = FastAPIUsers(user_db, [jwt_authentication], User, UserCreate, UserUpdate, UserDB, )
+fastapi_users = auth.FastAPIUsers(auth.user_db, [jwt_authentication], User, auth.UserCreate, auth.UserUpdate,
+                                  auth.UserDB, )
 app.include_router(fastapi_users.get_auth_router(jwt_authentication), prefix="/auth/jwt", tags=["auth"])
-app.include_router(fastapi_users.get_register_router(on_after_register), prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_reset_password_router(SECRET, after_forgot_password=on_after_forgot_password),
-                   prefix="/auth", tags=["auth"], )
-app.include_router(fastapi_users.get_verify_router(SECRET, after_verification_request=after_verification_request),
-                   prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(auth.on_after_register), prefix="/auth", tags=["auth"])
+app.include_router(
+    fastapi_users.get_reset_password_router(SECRET, after_forgot_password=auth.on_after_forgot_password),
+    prefix="/auth", tags=["auth"], )
+app.include_router(
+    fastapi_users.get_verify_router(SECRET, after_verification_request=auth.after_verification_request),
+    prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
-
-app.state.database = database
-
 verified_user = fastapi_users.current_user(verified=True)
 
-@app.on_event("startup")
-async def startup() -> None:
-    database_ = app.state.database
-    if not database_.is_connected:
-        await database_.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown() -> None:
-    database_ = app.state.database
-    if database_.is_connected:
-        await database_.disconnect()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/api/read-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
 async def update_item(subject: str, classroom: str, id: str, user: User = Depends(verified_user)):
@@ -114,9 +38,5 @@ async def update_item(subject: str, classroom: str, id: str, user: User = Depend
 @app.post("/api/add-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
 async def update_item(subject: str, classroom: str, id: str, lone: str = Form(default=None),
                       ltwo: str = Form(default=None), user: User = Depends(verified_user)):
-    return datahandler.save(subject, classroom, id, lone, ltwo)
-
-
-@app.get('/unr')
-def index():
-    return fastapi_users.current_user
+    datahandler.save(subject, classroom, id, lone, ltwo)
+    return {datahandler.response}
