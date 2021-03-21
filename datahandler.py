3abb61fa6dd7d5e88972d4fcc8a374894c37a2dc
mawoka <mawoka@e.email>
2021-03-21 11:34:36 +0100

    Finished the index

--- datahandler.py
+++ datahandler.py
@@ -1,21 +1,51 @@
 import os
 from fastapi import Form
-
+import pickle
+import json
+import re
+from collections import OrderedDict
 global response
 response = "hallo"
+
+
+#def savetoindex(path):
+#        with open(os.path.join('data/vocab/index.json'), "rb") as f:
+#            index = pickle.load(f)
+#        if index != "":
+#            with open(os.path.join('data/vocab/index.json'), "wb") as f:
+#                index = [index, "data/vocab/" + path]
+#                pickle.dump(index, f)
+#        else:
+#            with open(os.path.join('data/vocab/index.json'), "wb") as f:
+#                index = ["data/vocab/" + path]
+#                pickle.dump(index, f)
+#        return "success"
+
+def savetoindex(path):
+    with open(os.path.join('data/vocab/index.json'), "a") as f:
+        f.writelines(path + "\n")
+
+    with open(os.path.join('data/vocab/index.json'), "r") as f:
+        lines = f.readlines()
+        lines_set = set(lines)
+    with open(os.path.join('data/vocab/index.json'), "w") as f:
+        for line in lines_set:
+            f.write(line)
 
 
 def save(subject, classroom, id, l1, l2):
     try:
         os.mkdir("./data/vocab/" + classroom)
         os.mkdir("./data/vocab/" + classroom + "/" + subject)
-        f = open(os.path.join('./data/' + classroom + '/' + subject, id + ".txt"), "a")
+        f = open(os.path.join('./data/vocab/' + classroom + '/' + subject + '/ '+ id + ".txt"), "a")
         # f = open(os.path.join('./data' + classroom, id + ".txt"), "a")
         f.write(l1)
         f.write(" : ")
         f.write(l2)
         f.write("\n")
         f.close()
+        fullpath = classroom + "/" + subject + "/" + id + ".txt"
+        savetoindex(fullpath)
         response = "Success"
         print(response)
     except:
@@ -27,14 +57,14 @@ def save(subject, classroom, id, l1, l2):
             f.write(l2)
             f.write("\n")
             f.close()
+            fullpath = classroom + "/" + subject + "/" + id + ".txt"
+            savetoindex(fullpath)
             response = "Success"
             print(response)
         except:
             response = "Error"
             print(response)
 
-
-print(response)
 
 
 def read(subject, classroom, id):
@@ -48,6 +78,8 @@ def read(subject, classroom, id):
 
 
 def getcontent():
-    firstdir = os.listdir(path="data/vocab")
-    for i in firstdir:
-        print(i)
+    with open(os.path.join('data/vocab/index.json'), "r") as f:
+        index = f.read()
+    index = index.strip()
+    index = index.replace("\n", ";")
+    return index
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from typing import Optional
-from pydantic import BaseModel
 from fastapi import FastAPI, Form, Depends
-import datahandler
-import auth
+import datahandler, students,auth
 from auth import User, SECRET
 app = FastAPI()
 
@@ -30,13 +27,21 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/api/read-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
+@app.get("/api/vocab/read-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
 async def update_item(subject: str, classroom: str, id: str, user: User = Depends(verified_user)):
     return datahandler.read(subject, classroom, id)
 
 
-@app.post("/api/add-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
+@app.post("/api/vocab/add-list/{subject}/{classroom}/{id}", tags=["vocabapi"])
 async def update_item(subject: str, classroom: str, id: str, lone: str = Form(default=None),
                       ltwo: str = Form(default=None), user: User = Depends(verified_user)):
     datahandler.save(subject, classroom, id, lone, ltwo)
-    return {datahandler.response}
+    return datahandler.response
+
+@app.get("/api/vocab/list-list", tags=["vocabapi"])
+async def index(user: User = Depends(verified_user)):
+    return datahandler.getcontent()
+
+@app.post("/api/students/get-stats")
+async def index(user: User = Depends(verified_user)):
+
