ef2c351c88688040dcd03743448081abb99a5bfd
mawoka <mawoka-myblock@e.email>
2021-05-06 17:21:17 +0200

    Some progress on the learning screen!

--- interface/learn.py
+++ interface/learn.py
@@ -12,32 +12,39 @@ from icecream import ic
 
 
 def getvocab(classroom, subject, id):
-    response = requests.get(f'{geturl()}/api/vocab/read-list/{subject}/{classroom}/{id}', headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
+    global subject_word
+    global german
+    response = requests.get(f'{geturl()}/api/vocab/read-list/{subject}/{classroom}/{id}',
+                            headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
     vocab_undone = json.loads(response.text)
-    vocab = random.sample(vocab_undone.items(), k=len(vocab_undone))
-    return dict(vocab)
+    german = []
+    subject_word = []
+    for items in vocab_undone.items():
+        german.append(items[0]), subject_word.append(items[1])
+    random.shuffle(german)
+    random.shuffle(subject_word)
 
 
 def getstats(subject):
-    response = requests.get(f'{geturl()}/api/students/get-stats/{subject}/', headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
+    response = requests.get(f'{geturl()}/api/students/get-stats/{subject}/',
+                            headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
+    return response.json()
+
+@use_scope("First_scope")
+def inputgroup(subject):
+    for i in range(len(german)):
+        input_group(f"Please enter the {subject} word!", [input(f"Input the {german[i]} word", name="word")])
+        put_text(i)
 
 
 def index(subject, classroom):
-
-    response = requests.get(f'{geturl()}/api/vocab/list-list/{subject}/{classroom}', headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
+    response = requests.get(f'{geturl()}/api/vocab/list-list/{subject}/{classroom}',
+                            headers={'accept': 'application/json', 'Authorization': f'Bearer {ui.token}'})
 
     with use_scope('First_Scope', clear=True):
         what_to_do = input("Bitte wähle das Kapitel aus!", datalist=json.loads(response.text))
-        vocab = getvocab(classroom, subject, what_to_do)
-        put_text(vocab)
-        for i in vocab:
-            input_group(f"Please enter the {subject} word!",[put_text(vocab.key[1]), input(f"Input the {subject} word", name="word")])
-
-
-
-
-
-
+        getvocab(classroom, subject, what_to_do)
+        inputgroup(subject)
 
 
 
